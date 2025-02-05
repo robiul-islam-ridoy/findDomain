32df2eb5a874f05b48c9d0783614af995156831d
Robiul Islam <robiulislamhr.bd@gmail.com>
2025-02-06 00:30:06 +0600

    functionality added

--- substring.java
+++ substring.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class substring {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String email;
+        String userName;
+        String domain;
+
+        System.out.println("Enter Your Email: ");
         
+        email = sc.nextLine();
+
+        userName = email.substring(0, email.indexOf("@"));
+        domain = email.substring(email.indexOf("@") + 1);
+
+        System.out.println("User Name: " + userName);
+        System.out.println("Domain: " + domain);
     }
 }
